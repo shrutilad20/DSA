463f8dc02e776d7f94e61852acfe6046f4a4785b
Shruti Prashant Lad <150305068+shrutilad20@users.noreply.github.com>
2025-06-28 20:46:04 +0530

    Create BestTimetoBuyandSellStock.java

---
+++ BestTimetoBuyandSellStock.java
@@ -0,0 +1,27 @@
+121. Best Time to Buy and Sell Stock
+
+  class Solution {
+    public int maxProfit(int[] arr) {
+        int min=Integer.MAX_VALUE;
+        int max=0;
+        
+        for(int i=0;i<arr.length;i++){
+                    
+
+            if(arr[i]<min){
+                min=arr[i];
+            }
+
+           else  if(arr[i]-min >max)
+                {
+                   max = arr[i]-min;
+                }
+
+
+            
+        }
+    
+            return max;
+
+}}
+  
