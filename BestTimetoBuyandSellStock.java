9e165673f7d15810887096c8d8d71ab0344d17a1
Shruti Prashant Lad <150305068+shrutilad20@users.noreply.github.com>
2025-10-17 16:46:47 +0530

    Remove unnecessary blank lines in stock analysis

--- BestTimetoBuyandSellStock.java
+++ BestTimetoBuyandSellStock.java
@@ -6,8 +6,7 @@
         int max=0;
         
         for(int i=0;i<arr.length;i++){
-                    
-
+                  
             if(arr[i]<min){
                 min=arr[i];
             }
